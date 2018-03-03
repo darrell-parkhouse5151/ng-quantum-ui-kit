91e2d69b9396d4307f4f31f4906cc827e3a0fb8f
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-03 11:37:26 -0600

    the show errors class

--- src/app/show-errors/show-errors.component.ts
+++ src/app/show-errors/show-errors.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+/* tslint:disable*/
+import { Component, Input } from '@angular/core';
+import { AbstractControlDirective, AbstractControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-show-errors',
-  templateUrl: './show-errors.component.html',
-  styleUrls: ['./show-errors.component.scss']
+    selector: 'app-show-errors',
+    templateUrl: './show-errors.component.html',
+    styleUrls: ['./show-errors.component.scss']
 })
-export class ShowErrorsComponent implements OnInit {
+export class ShowErrorsComponent {
+    private static readonly errorMessages = {
+        'required': () => 'This field is required',
+        'minlength': (params) => 'this min number of characters is ' + params.requiredLength,
+        'maxlength': (params) => 'the max allowed number of characters is ' + params.requiredLength,
+        'pattern': (params) => 'the required patter is :' + params.requiredPattern,
+        'years': (params) => params.message,
+        'countryCity': (params) => params.message,
+        'uniqueName': (params) => params.message,
+        'telephoneNumbers': (params) => params.message,
+        'telephoneNumber': (params) => params.message,
+    };
 
-  constructor() { }
+    @Input()
+    private control: AbstractControlDirective | AbstractControl;
 
-  ngOnInit() {
-  }
+    shouldShowErrors(): boolean {
+        return this.control && this.control.errors && (this.control.dirty || this.control.touched);
+    }
+
+    listOfErrors(): string[] {
+        return Object.keys(this.control.errors)
+            .map(field => this.getMessage(field, this.control.errors[field]));
+    }
+
+    private getMessage(type:string, params: any) {
+        return ShowErrorsComponent.errorMessages[type](params);
+    }
 
 }
