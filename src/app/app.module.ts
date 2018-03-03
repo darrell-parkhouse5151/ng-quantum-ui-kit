258bdcb3427c71b6d6b7cf0af8ef8ac8fa61f3ad
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-03 14:17:03 -0600

    added the form-validation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,23 @@ import { AppComponent } from './app.component';
 import { ShowErrorsComponent } from './show-errors/show-errors.component';
 
 import { FormsModule } from "@angular/forms";
+import { FormValidationComponent } from './form-validation/form-validation.component';
+import { BirthYearValidationDirective } from './directives/birth-year.directive';
+import { CountryCityValidationDirective} from "./directives/country-city.directive";
+import { TelephoneNumberValidatorDirective } from './directives/telephone-number-validator.directive';
+import { UniqueNameValidatorDirective } from './directives/unique-name-validator.directive';
+import { ReactiveFormValidation } from "./reactive-form-validation/reactive-form-validation";
 
 @NgModule({
     declarations: [
         AppComponent,
-        ShowErrorsComponent
+        ShowErrorsComponent,
+        FormValidationComponent,
+        BirthYearValidationDirective,
+        CountryCityValidationDirective,
+        TelephoneNumberValidatorDirective,
+        UniqueNameValidatorDirective,
+        ReactiveFormValidation
     ],
     imports: [
         BrowserModule,
