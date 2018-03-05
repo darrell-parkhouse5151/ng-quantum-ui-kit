45ab202b424fd21665c8d028e7578f84f4e0d9c2
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-05 04:30:29 -0600

    removed unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,26 +7,12 @@ import { AppComponent } from './app.component';
 import { ShowErrorsComponent } from './show-errors/show-errors.component';
 
 import { FormsModule } from "@angular/forms";
-import { FormValidationComponent } from './form-validation/form-validation.component';
-import { BirthYearValidationDirective } from './directives/birth-year.directive';
-import { CountryCityValidationDirective} from "./directives/country-city.directive";
-import { TelephoneNumberValidatorDirective } from './directives/telephone-number-validator.directive';
-import { UniqueNameValidatorDirective } from './directives/unique-name-validator.directive';
-import { ReactiveFormValidation } from "./reactive-form-validation/reactive-form-validation";
-import { CustomValidatorsComponent } from './custom-validators/custom-validators.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         ShowErrorsComponent,
-        FormValidationComponent,
-        BirthYearValidationDirective,
-        CountryCityValidationDirective,
-        TelephoneNumberValidatorDirective,
-        UniqueNameValidatorDirective,
-        ReactiveFormValidation,
-        CustomValidatorsComponent,
         NavbarComponent
     ],
     imports: [
