f8f7062cfb152e214ba9f8eadffa42a78eb90384
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-05 03:58:13 -0600

    added required and minLength methods

--- src/app/custom-validators/custom-validator.ts
+++ src/app/custom-validators/custom-validator.ts
@@ -1,89 +1,28 @@
+/* tslint:disable*/
 import {AbstractControl, FormArray, FormControl, FormGroup, ValidationErrors} from "@angular/forms";
 
 export class CustomValidator {
-    register({value, valid}) {}
-    static birthYear(c: FormControl): ValidationErrors {
-        const numValue = Number(c.value);
-        const currentYear = new Date().getFullYear();
-        const minYear = currentYear - 85;
-        const maxYear = currentYear - 18;
-        const isValid = !isNaN(numValue) && numValue >= minYear && numValue <= maxYear;
+    static required(c: FormControl) : Promise<ValidationErrors> {
         const message = {
-            'year': {
-                'message': 'the year must be a valid number between' + minYear + ' and '
-            }
-        };
-        return isValid ? null : message;
-    }
-
-    static countryCity(form: FormGroup):ValidationErrors {
-        const countryControl = form.get('location.country'),
-            cityControl = form.get('location.city');
-
-        if (countryControl != null && cityControl != null) {
-            const country = countryControl.value;
-            const city = cityControl.value;
-            let error = null;
-
-            if (country === 'France' && city !== 'paris') {
-                error = 'if the country is france, the city must be paris'
-            }
-
-            const message = {
-                'countryCity': {
-                    'message': error
-                }
-            };
-
-            return error ? message : null;
-        }
-    }
-
-    static uniqueName(c?: FormControl):Promise<ValidationErrors> {
-        const message = {
-            'uniqueName': {
-                'message': 'this name is not unique'
+            'required': {
+                'message': 'this field is required'
             }
         };
 
         return new Promise(resolve => {
             setTimeout(() => {
-                resolve(c.value === 'existing' ? message : null);
-            }, 1000);
+                resolve(c.value === '' ? message : null);
+            })
         });
     }
 
-    static telephoneNumber(c:FormControl): ValidationErrors {
-        const isValidPhoneNumber = /^\d{3,3}-\d{3,3}-\d{3,3}$/.test(c.value);
-        const message = {
-            'telephoneNumber': {
-                'message': 'the phone number must be valid (xxx-xxx-xxxx, where is is a digit'
-            }
-        };
+    static minLength(value: number, c?:FormControl) : Promise<ValidationErrors> {
+        const message = {};
 
-        return isValidPhoneNumber ? null : message;
-    }
-
-    static telephoneNumbers(form?: FormGroup): ValidationErrors {
-        const message = {
-            'telephoneNumbers': {
-                'message': 'at least one telephone number must be entered'
-            }
-        };
-
-        const phoneNumbers = <FormArray>form.get('phoneNumbers');
-        const hasPhoneNumbers = phoneNumbers && Object.keys(phoneNumbers.controls).length > 0;
-
-        return hasPhoneNumbers ? null : message;
-    }
-
-    static buildPhoneNumberComponent(c?: AbstractControl):AbstractControl {
-        if (c === null) {
-            return c
-        } else {
-            return null;
-        }
-
-
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(c.value <= value ? message : null);
+            });
+        });
     }
 }
