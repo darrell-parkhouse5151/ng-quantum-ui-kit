e1e196ad9e2ef197804bf9f7630167fc3f55982d
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-05 05:22:33 -0600

    the cli added email, leads and contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,21 @@ import { ShowErrorsComponent } from './show-errors/show-errors.component';
 import { FormsModule } from "@angular/forms";
 import { NavbarComponent } from './navbar/navbar.component';
 import { VertNavComponent } from './vert-nav/vert-nav.component';
+import { DashboardComponent } from './navbar/dashboard/dashboard.component';
+import { LeadsComponent } from './navbar/dashboard/leads/leads.component';
+import { ContactsComponent } from './navbar/dashboard/contacts/contacts.component';
+import { EmailComponent } from './navbar/dashboard/email/email.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         ShowErrorsComponent,
         NavbarComponent,
-        VertNavComponent
+        VertNavComponent,
+        DashboardComponent,
+        LeadsComponent,
+        ContactsComponent,
+        EmailComponent
     ],
     imports: [
         BrowserModule,
