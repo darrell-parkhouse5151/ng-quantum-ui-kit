a93e15b67f711b7d44395c635abdb89dc172a213
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-05 05:18:36 -0600

    this is a dumb component there is not need for logic

---
+++ src/app/vert-nav/vert-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vert-nav',
+  templateUrl: './vert-nav.component.html',
+  styleUrls: ['./vert-nav.component.scss']
+})
+export class VertNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
