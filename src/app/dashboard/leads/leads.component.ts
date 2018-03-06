0826eae8ad50c678b968f0c0c90bb6d8f935aaac
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-06 02:48:59 -0600

    connects to the leads firebase databae

---
+++ src/app/dashboard/leads/leads.component.ts
@@ -0,0 +1,23 @@
+/* tslint:disable*/
+import { Component, OnInit } from '@angular/core';
+import { LeadService} from '../../services/leads.service';
+import { Lead } from '../../models/lead';
+
+@Component({
+    selector: 'app-leads',
+    templateUrl: './leads.component.html',
+    styleUrls: ['./leads.component.scss']
+})
+export class LeadsComponent implements OnInit {
+    leads: Lead[];
+    constructor(private leadService: LeadService) {
+    }
+
+    ngOnInit() {
+        this.leadService.getLeads().subscribe(leads => {
+            this.leads = leads;
+        });
+    }
+
+
+}
