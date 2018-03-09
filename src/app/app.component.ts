a2c5720c699b5e8b0c95d4e8b5feb82294a3797e
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-09 09:14:15 -0600

    removed unneeded component references

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+/* tslint:disable*/
+import {Component} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
+import {Lead} from './models/lead';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+    constructor() {
+
+    }
 }
