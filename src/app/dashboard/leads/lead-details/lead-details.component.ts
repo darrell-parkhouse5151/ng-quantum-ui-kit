b2617cc8e8bdd3e751f0c0fbd0b3f1958b9b06d7
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-09 11:00:42 -0600

    add the onDelete() method

---
+++ src/app/dashboard/leads/lead-details/lead-details.component.ts
@@ -0,0 +1,33 @@
+/* tslint:disable*/
+import { Component, OnInit } from '@angular/core';
+import {LeadService} from '../../../services/leads.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Lead} from '../../../models/lead';
+
+@Component({
+    selector: 'app-lead-details',
+    templateUrl: './lead-details.component.html',
+    styleUrls: ['./lead-details.component.scss']
+})
+export class LeadDetailsComponent implements OnInit {
+    id:string;
+    lead: Lead;
+
+    constructor(private leadService: LeadService,
+                private router: Router,
+                private route: ActivatedRoute) {
+    }
+
+    ngOnInit() {
+        this.id = this.route.snapshot.params['id'];
+
+        this.leadService.getLead(this.id).subscribe(lead => this.lead = lead;)
+    }
+
+    onDelete() {
+        if (confirm('Are you sure')) {
+            this.leadService.deleteLead(this.lead);
+        }
+    }
+
+}
