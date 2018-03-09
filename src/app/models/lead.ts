20047fc8acb64021fe1b39882acf79d490b203af
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-09 08:50:00 -0600

    added the leads model

---
+++ src/app/models/lead.ts
@@ -0,0 +1,11 @@
+/* tslint:disable*/
+export interface Lead {
+    id?:string;
+    company:string;
+    contactName:string;
+    leadStatus:string;
+    leadTitle:string;
+    sales:number;
+    backgroundColor:string;
+
+}
