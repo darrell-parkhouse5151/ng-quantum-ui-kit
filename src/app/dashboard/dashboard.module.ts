4bfb0f6ce7d32e33de7bde016196c203df62dcd3
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-12 01:50:24 -0500

    custom module for the dashboard component

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { EmailClientComponent } from './email-client/email-client.component';
+import { LeadsComponent } from './leads/leads.component';
+import { AddLeadComponent } from './leads/add-lead/add-lead.component';
+import { LeadDetailsComponent } from './leads/lead-details/lead-details.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
+    declarations: [
+        EmailClientComponent,
+        LeadsComponent,
+        AddLeadComponent,
+        LeadDetailsComponent,
+        ContactsComponent
+    ],
     imports: [
         CommonModule,
         DashboardRoutingModule
     ]
 })
 
-export class DashboardModule { }
+export class DashboardModule {
+}
