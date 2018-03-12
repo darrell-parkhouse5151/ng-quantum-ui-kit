74f288ac0ab353473a9e38f92e9fec6e8ce4865e
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-12 01:26:18 -0500

    custom module for the basic elements

---
+++ src/app/basic-elements/basic-elements.module.ts
@@ -0,0 +1,12 @@
+/* tslint:disable*/
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BasicElementsRoutingModule } from './basic-elements-routing.module';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BasicElementsRoutingModule
+    ]
+})
+export class BasicElementsModule {}
