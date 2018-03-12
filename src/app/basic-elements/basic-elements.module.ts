89e85be48c5bd17e7599d145cd05cb780af549d4
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-12 01:48:39 -0500

    custom module for basic elements

--- src/app/basic-elements/basic-elements.module.ts
+++ src/app/basic-elements/basic-elements.module.ts
@@ -2,11 +2,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasicElementsRoutingModule } from './basic-elements-routing.module';
+import { AlertsComponent } from './alerts/alerts.component';
 
 @NgModule({
     imports: [
         CommonModule,
         BasicElementsRoutingModule
+    ],
+    declarations: [
+        AlertsComponent
     ]
 })
-export class BasicElementsModule {}
+export class BasicElementsModule {
+}
