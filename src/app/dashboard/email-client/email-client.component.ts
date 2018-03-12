93020ab7d1cd17563fe5a7fdb16be559ca76965d
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-12 00:44:12 -0500

    added the pageName and componentName for the breadcrumbs

--- src/app/dashboard/email-client/email-client.component.ts
+++ src/app/dashboard/email-client/email-client.component.ts
@@ -1,15 +1,18 @@
+/* tslint:disable*/
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-email-client',
-  templateUrl: './email-client.component.html',
-  styleUrls: ['./email-client.component.scss']
+    selector: 'app-email-client',
+    templateUrl: './email-client.component.html',
+    styleUrls: ['./email-client.component.scss']
 })
 export class EmailClientComponent implements OnInit {
+    pageName: string = 'Email Client';
+    componentName: string = 'Mailbox CRM';
+    constructor() {
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
