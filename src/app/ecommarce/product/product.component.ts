7f765905b1ae3f3ebfecb2e3126f729dde130a3e
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-13 17:41:15 -0500

    logic for the product component

---
+++ src/app/ecommarce/product/product.component.ts
@@ -0,0 +1,20 @@
+/* tslint:disable*/
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
+
+@Component({
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+    products: Product[];
+
+    constructor(private productService: ProductService) { }
+
+    ngOnInit() {
+        this.productService.getProducts().subscribe(products => this.products = products);
+    }
+
+}
