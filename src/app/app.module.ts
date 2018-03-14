535ba04b5d03be8afda4f0677ec60ff74553e285
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-13 19:05:12 -0500

    cli added the shopping-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ShowErrorsComponent } from './show-errors/show-errors.component';
 
-import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
 import { VertNavComponent } from './vert-nav/vert-nav.component';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 import { HttpClientModule } from '@angular/common/http';
-import { TestComponent } from './test-component/test.component';
-import { ProductDetailsComponent } from './ecommarce/product-details/product-details.component';
-import { ProductAddComponent } from './ecommarce/product-add/product-add.component';
-import { ProductComponent } from './ecommarce/product/product.component';
+import { TestComponent } from './basic-elements/test-component/test.component';
+import { ProductDetailsComponent } from './ecommerce/product-details/product-details.component';
+import { ProductAddComponent } from './ecommerce/product-add/product-add.component';
+import { ProductComponent } from './ecommerce/product/product.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { ServicesModule } from './services/services.module';
 import { BasicElementsComponent } from './basic-elements/basic-elements.component';
 import { BasicElementsModule } from './basic-elements/basic-elements.module';
+import { DashboardRoutingModule } from './dashboard/dashboard-routing.module';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { ProfilesComponent } from './social/profiles/profiles.component';
+import { CartComponent } from './ecommerce/cart/cart.component';
+import { ShoppingCartComponent } from './ecommerce/shopping-cart/shopping-cart.component';
 
 
 @NgModule({
@@ -32,18 +36,23 @@ import { BasicElementsModule } from './basic-elements/basic-elements.module';
         ProductAddComponent,
         ProductComponent,
         BasicElementsComponent,
+        ProfilesComponent,
+        CartComponent,
+        ShoppingCartComponent,
     ],
     imports: [
         BrowserModule,
-        FormsModule,
         DashboardModule,
         HttpClientModule,
         ServicesModule,
         DashboardModule,
+        DashboardRoutingModule,
         BasicElementsModule,
         AngularFireModule.initializeApp(environment.firebaseConfig, 'ng-quantum-ui-kit')
     ],
-    providers: [],
+    providers: [
+        AngularFirestore
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
