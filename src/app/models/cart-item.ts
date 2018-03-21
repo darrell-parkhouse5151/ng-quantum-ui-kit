5d2a39e712977a87f0ac70bcc3bb5766f4a3a702
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-21 01:34:27 -0500

    added the ShoppingCart class

--- src/app/models/cart-item.ts
+++ src/app/models/cart-item.ts
@@ -3,3 +3,35 @@ export class CartItem {
     public productId: string;
     public quantity: number;
 }
+
+export interface Comparable {
+    equals();
+}
+
+export interface CartItem extends Comparable {
+    setQuantity(item: CartItem, quantity?: number);
+    getQuantity();
+}
+
+export interface Cart {
+    add(item: CartItem);
+    remove(item: CartItem);
+    getQuantity(item: CartItem);
+    setQuantity(item: CartItem, quantity: number);
+    isEmpty();
+    getItems();
+    clear();
+}
+
+export class ShoppingCart implements Cart {
+    add(item: CartItem) {
+
+    }
+
+    remove(item: CartItem) {}
+    getQuantity(item: CartItem) {}
+    setQuantity(item: CartItem, quantity: number) {}
+    isEmpty() {}
+    getItems() {}
+    clear() {}
+}
