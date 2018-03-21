eed53a27c0afdb2d610a1c643abdfaa226c17977
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-21 01:38:49 -0500

    added pageName and the componentName for the breadcrumbs

--- src/app/ecommerce/cart/cart.component.ts
+++ src/app/ecommerce/cart/cart.component.ts
@@ -1,8 +1,5 @@
 /* tslint:disable*/
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {isObject} from 'rxjs/util/isObject';
-import {isString} from 'util';
-import {Cart} from '../../models/cart';
+import {Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-cart',
@@ -10,31 +7,13 @@ import {Cart} from '../../models/cart';
     styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-    @Output() itemUpdated = new EventEmitter();
-    public cart = {
-        quantity: 0,
-        total: 0,
-        tax: 0,
-        numberOfItems: [],
-        subTotal: 0,
-        id: ''
-    };
+    pageName: string = 'Cart';
+    componentName: string = 'Ecommerce Cart';
     constructor() {
     }
 
     ngOnInit() {
     }
 
-    addItem(id, name, price, quantity, data) {
-        let inCart = this.getItemById(id);
-
-        if (typeof inCart === 'object') {
-
-        }
-    }
-
-    getItemById(id) {
-
-    }
 
 }
