6625c91c43de51d9c913576a216518947771fd3f
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-26 16:37:35 -0500

    added a few constants

---
+++ src/app/utils/constants.ts
@@ -0,0 +1,22 @@
+/* tslint:disable*/
+const HOST = 'http://localhost:3000';
+
+export const CONSTANTS = {
+    permission: {},
+    routes: {
+        authorization: {
+            login: HOST + '/api/auth/login',
+            logout: HOST + '/api/auth/logout'
+        },
+        person: {
+            list: HOST + '/api/person',
+            delete: HOST + '/api/person/delete/:id',
+            save: HOST + '/api/person/save'
+        },
+
+        user: {}
+    },
+    lang: {},
+    session: {},
+    parameters: {}
+};
